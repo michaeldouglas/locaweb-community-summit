881476ff29a04e0ec89cfa5c4e6e1ff138ea5c65
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2023-09-03 14:06:43 -0300

    Adjustments structure

--- codigos/src/TotalParticipantes.php
+++ codigos/src/TotalParticipantes.php
@@ -15,13 +15,35 @@ if (($handle = fopen($csvFile, "r")) !== false) {
 // Calcular o número total de dados
 $total = count($data);
 
-// Exibir o número total de dados
+// Obtem as respostas
+$responses = [];
+foreach ($data as $row) {
+    $split = explode(',', $row[0]);
+    $responses[] = $split;
+}
+$columnIndex = 4;
+$columnData = array_column($responses, $columnIndex);
 
-$html_table = '<table style="border-collapse: collapse; border-spacing: 0; border: 1px solid black; width: 30%;">';
-$html_table .= '<tr style="border: 1px solid black; background-color: #f2f2f2; color: #000;"><th style="border: 1px solid black; padding: 8px;">Total de participantes</th></tr>';
+// Exibir o total
+$html_table = '<table style="border-collapse: collapse; border-spacing: 0; border: 1px solid black; width: 100%;">';
+$html_table .= '<tr style="border: 1px solid black; background-color: #f2f2f2; color: #000;">
+<th style="border: 1px solid black; padding: 8px;">Total de participantes</th>
+<th style="border: 1px solid black; padding: 8px;">Total voltaria no evento</th>
+</tr>';
+
+$total = count($columnData);
+if ($total > 0) {
+  $totalVoltaria = array_sum(array_map(function ($x) {
+    return strtolower($x) === 'sim' ? 1 : 0;
+  }, $columnData));
+}
 
 $html_table .= '<tr style="border: 1px solid black;">
-<td style="border: 1px solid black; padding: 8px;">' . $total . '</td></tr>';
+  <td style="border: 1px solid black; padding: 8px;">' . $total . '</td>
+</tr>';
+$html_table .= '<tr style="border: 1px solid black;">
+  <td style="border: 1px solid black; padding: 8px;">' . $totalVoltaria . '</td>
+</tr>';
 
 $html_table .= '</table>';
 
