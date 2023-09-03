443dc8a364c13d9098a5dca5046d12d57a4b4553
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2023-09-03 13:21:08 -0300

    Adjustments structure

---
+++ codigos/src/TotalParticipantes.php
@@ -0,0 +1,19 @@
+<?php
+// Ler o arquivo CSV
+$csvFile = __DIR__ . '/../dataset/Participantes.csv';
+$data = [];
+if (($handle = fopen($csvFile, "r")) !== false) {
+    // Remover a primeira linha (cabeçalho)
+    fgetcsv($handle, 1000, ";");
+    
+    while (($row = fgetcsv($handle, 1000, ";")) !== false) {
+        $data[] = $row;
+    }
+    fclose($handle);
+}
+
+// Calcular o número total de dados
+$total = count($data);
+
+// Exibir o número total de dados
+echo "Total de participantes: " . $total . "\n\n";
--- codigos/src/importacaoDados.php
+++ codigos/src/importacaoDados.php
@@ -15,13 +15,6 @@ if (($handle = fopen($csvFile, "r")) !== false) {
 // Calcular o número total de dados
 $total = count($data);
 
-// Exibir o número total de dados
-echo "Total de participantes: " . $total . "\n\n";
-
-// Exibir as primeiras 5 linhas do DataFrame
-echo "Exibe os 5 primeiros participantes: " . "\n";
-$head = array_slice($data, 0, 5);
-
 // Criar uma tabela em Markdown com colunas separadas
 $markdown_table = "| NOME | IDADE | ESTADO | CIDADE | VOLTARIA_NO_EVENTO |\n";
 $markdown_table .= "|------|-------|--------|--------|---------------------|\n";
