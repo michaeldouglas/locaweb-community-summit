12ee8f8258fa081f5ae6efb4f8bcc80da49daecf
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2023-09-03 12:04:22 -0300

    Ajuste do projeto

---
+++ codigos/src/importacaoDados.php
@@ -0,0 +1,25 @@
+<?php
+
+// Ler o arquivo CSV
+
+$csvFile = __DIR__ . '/../dataset/Participantes.csv';
+$data = [];
+if (($handle = fopen($csvFile, "r")) !== false) {
+    while (($row = fgetcsv($handle, 1000, ";")) !== false) {
+        $data[] = $row;
+    }
+    fclose($handle);
+}
+
+// Calcular o número total de dados
+$total = count($data);
+
+// Exibir o número total de dados
+echo "Total de participantes: " . $total . "\n";
+
+// Exibir as primeiras 5 linhas do DataFrame
+$head = array_slice($data, 0, 5);
+foreach ($head as $row) {
+    echo implode(", ", $row) . "\n";
+}
+?>
