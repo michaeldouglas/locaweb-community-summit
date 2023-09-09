d401efc79ab03a508a3fb3d187c2552a59df197f
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2023-09-09 17:47:30 -0300

    Adjustments structure

--- codigos/src/ComParametros.php
+++ codigos/src/ComParametros.php
@@ -1,6 +1,12 @@
 <?php
+// Abre a entrada padrão em modo leitura
+$stdin = fopen('php://stdin', 'r');
+
 // Lê os dados da entrada padrão (stdin)
-$dados_do_python = file_get_contents("php://stdin");
+$dados_do_python = stream_get_contents($stdin);
+
+// Fecha a entrada padrão
+fclose($stdin);
 
 // Imprime os dados recebidos
 echo "Dados recebidos do Python:\n";
