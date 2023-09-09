be56a65e523ea596bf393ee1c9830e89b7660f42
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2023-09-09 18:06:51 -0300

    Adjustments structure

--- codigos/src/ComParametros.php
+++ codigos/src/ComParametros.php
@@ -45,3 +45,91 @@ $cityMapping = [
   'JUNDIAI' => 4
 ];
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resultados</title>
+    <style>
+        /* Estilo para tornar a tabela responsiva */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            border: 1px solid #000; /* Adicionar borda à tabela */
+        }
+
+        /* Estilo para alternar cores das linhas */
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+            color: black;
+        }
+
+        /* Estilo para os cabeçalhos com cores diferentes */
+        th {
+            background-color: #f2f2f2;
+            color: black;
+        }
+
+        /* Estilo para as células da tabela */
+        td, th {
+            padding: 8px;
+            border: 1px solid #000;
+            text-align: left;
+        }
+        .green {
+            color: green;
+            font-weight: bold;
+        }
+        .red {
+            color: red;
+            font-weight: bold;
+        }
+        .justify {
+            align-items: center;
+            display: flex;
+        }
+        .justify i {
+            margin-left: 0.3rem;
+        }
+    </style>
+    <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+</head>
+<body>
+<div style="text-align: center;"><img src='https://php.locaweb.com.br/wp-content/themes/locaweb-php-community-2023/svg/logo.svg' alt='php-community-2023' /><div><br />
+    <table border="1">
+        <tr>
+            <th>Idade</th>
+            <th>Estado</th>
+            <th>Cidade</th>
+            <th>Resultado</th>
+        </tr>
+
+        <!-- Exiba as previsões em uma tabela HTML -->
+        <?php foreach ($predictions as $index => $prediction) : ?>
+            <?php
+            $testSample = $testData[$index];
+            $idade = $testSample[0];
+            $estado = array_search($testSample[1], $stateMapping); // Converter de volta para o nome do estado
+            $cidade = array_search($testSample[2], $cityMapping); // Converter de volta para o nome da cidade
+            $result = $prediction === 'SIM' ? 'Retornaria ao evento' : 'Não retornaria ao evento';
+            $class = $prediction === 'SIM' ? 'green' : 'red';
+            $icon = $prediction === 'SIM' ? 'check' : 'block';
+            ?>
+            <tr>
+                <td class="<?php echo $class; ?>"><?php echo $idade; ?></td>
+                <td class="<?php echo $class; ?>"><?php echo $estado; ?></td>
+                <td class="<?php echo $class; ?>"><?php echo $cidade; ?></td>
+                <td class="<?php echo $class; ?>">
+                    <div class="justify">
+                        <?php echo $result; ?> <i class="material-icons"><?php echo $icon; ?></i>
+                    </div>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <!-- Resto do seu código HTML -->
+</body>
+</html>
